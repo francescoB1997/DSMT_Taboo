da7f13e155eb10e8d7e738b9e6831b2b4fc300b2
francescoB1997 <francesco.0297@hotmail.it>
2024-02-10 09:39:01 +0100

    Aggiunta pagina index

--- src/main/java/it/unipi/dsmt/dsmt_taboo/DsmtTabooApplication.java
+++ src/main/java/it/unipi/dsmt/dsmt_taboo/DsmtTabooApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DsmtTabooApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DsmtTabooApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DsmtTabooApplication.class, args);
+    }
 
 }
--- src/test/java/it/unipi/dsmt/dsmt_taboo/DsmtTabooApplicationTests.java
+++ src/test/java/it/unipi/dsmt/dsmt_taboo/DsmtTabooApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DsmtTabooApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }

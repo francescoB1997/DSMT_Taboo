7dfed89a91f56053d3d9c2b24ec418dbda1056fe
g.sferr <94866345+g-sferr@users.noreply.github.com>
2024-02-23 17:44:13 +0100

    removeFriend method added for db

--- src/main/java/it/unipi/dsmt/dsmt_taboo/DAO/FriendDAO.java
+++ src/main/java/it/unipi/dsmt/dsmt_taboo/DAO/FriendDAO.java
@@ -13,31 +13,20 @@ public class FriendDAO extends BaseDAO
     // This class represents the friendships of a specific user -> username
 {
     private String username;
-    private List<FriendDTO> friendList;
-    private SessionManagement session;
 
     public FriendDAO(String username)
     {
         System.out.println("FriendDAO: " + username);
         this.username = username;
-        this.friendList = new ArrayList<>();
-        this.session = SessionManagement.getInstance();
     }
-
-    public void addFriend(FriendDTO friend) { this.friendList.add(friend); }
 
     public List<FriendDTO> getFriendList()
     {
-        this.getFriendListFromDB();
-        return this.friendList;
-    }
+        List<FriendDTO> friendList = new ArrayList<>();
 
-    public void setFriendList(List<FriendDTO> friendList) { this.friendList = friendList; }
-
-    private void getFriendListFromDB()
-    {
         String getAllFriendQuery = "SELECT CASE WHEN F.Username1 = ? then F.Username2 else F.Username1 END as FriendUsername FROM " +
                 DB_NAME + ".friendship as F where Username1 = ? OR Username2 = ? ";
+
         try (
                 Connection connection = getConnection();
                 PreparedStatement checkStatement = connection.prepareStatement(getAllFriendQuery))
@@ -50,27 +39,28 @@ public class FriendDAO extends BaseDAO
                 while (resultSet.next())
                 {
                     String friendUsername = resultSet.getString("FriendUsername");
-                    this.addFriend(new FriendDTO(friendUsername, this.session.isUserLogged(friendUsername)));
+                    friendList.add(new FriendDTO(friendUsername, SessionManagement.getInstance().isUserLogged(friendUsername)));
                 }
             }
         } catch (SQLException ex)
         {
-            System.out.println("getFriendListFromDB eccezione query: " + ex.getMessage());
-            return;
+            System.out.println("searchUserInDB eccezione query:" + ex.getMessage());
+            return  null;
         }
-    }
 
-    public boolean removeFriend(String username1, String username2) {
+        return friendList;
+    }
+    public boolean removeFriendDB(String usernameToRemove) {
         String removeQuery = "DELETE FROM " + DB_NAME + ".friendship " +
                 "WHERE (Username1 = ? AND Username2 = ?) OR (Username1 = ? AND Username2 = ?)";
-                //Indipendentemente dall'ordine dei nomi degli utenti
+        //Indipendentemente dall'ordine dei nomi degli utenti
 
         try (Connection connection = getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(removeQuery)) {
-            preparedStatement.setString(1, username1);
-            preparedStatement.setString(2, username2);
-            preparedStatement.setString(3, username2);
-            preparedStatement.setString(4, username1);
+            preparedStatement.setString(1, this.username);
+            preparedStatement.setString(2, usernameToRemove);
+            preparedStatement.setString(3, usernameToRemove);
+            preparedStatement.setString(4, this.username);
 
             return preparedStatement.executeUpdate() > 0;
         } catch (SQLException ex) {

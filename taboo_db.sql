527a9418c094cc708e3e33c835f99bc786631a69
g.sferr <94866345+g-sferr@users.noreply.github.com>
2024-04-05 14:05:51 +0200

    new DB dump added with new table Match

--- taboo_db.sql
+++ taboo_db.sql
@@ -1,11 +1,8 @@
-DROP DATABASE IF EXISTS `taboo`;
-CREATE DATABASE `taboo` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `taboo`;
--- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: taboo
+-- Host: localhost    Database: taboo
 -- ------------------------------------------------------
--- Server version	8.0.31
+-- Server version	8.0.36
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -26,9 +23,9 @@ DROP TABLE IF EXISTS `friendship`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `friendship` (
-                              `Username1` varchar(45) NOT NULL,
-                              `Username2` varchar(45) NOT NULL,
-                              PRIMARY KEY (`Username1`,`Username2`)
+  `Username1` varchar(45) NOT NULL,
+  `Username2` varchar(45) NOT NULL,
+  PRIMARY KEY (`Username1`,`Username2`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -38,12 +35,7 @@ CREATE TABLE `friendship` (
 
 LOCK TABLES `friendship` WRITE;
 /*!40000 ALTER TABLE `friendship` DISABLE KEYS */;
-INSERT INTO `friendship` VALUES ('alex_cook','ella_miller'),('alex_cook','mia_brown'),('ella_miller','luke_smith'),('emma_taylor','owen_hall'),('jackson84','max_wilson'),('jackson84','sarah_johnson'),
-                                ('fra','gsf'),('fra','noah_carter'),('fra','owen_hall'),('fra','zoe_jenkins'),
-                                ('fra','alex_cook'),('fra','ella_miller'),('fra','mia_brown'),
-                                ('fra','luke_smith'),('fra','emma_taylor'),('fra','max_wilson'),
-                                ('gsf','luke_smith'),
-                                ('max_wilson','emma_taylor'),('max_wilson','owen_hall'),('owen_hall','alex_cook'),('owen_hall','ella_miller'),('owen_hall','sarah_johnson'),('sarah_johnson','alex_cook'),('sarah_johnson','david_moore'),('sarah_johnson','jackson84'),('sophie_davis','david_moore'),('user123','jackson84'),('user456','lily22');
+INSERT INTO `friendship` VALUES ('alex_cook','ella_miller'),('alex_cook','mia_brown'),('cia','fra'),('cia','gsf'),('cia2','fra'),('cia2','gsf'),('ella_miller','luke_smith'),('emma_taylor','owen_hall'),('fra','ella_miller'),('fra','emma_taylor'),('fra','luke_smith'),('fra','max_wilson'),('fra','mia_brown'),('fra','noah_carter'),('fra','owen_hall'),('fra2','fra'),('fra2','gsf'),('g.allevi100','fra'),('g.allevi100','gsf'),('g.allevi13','amy_carter'),('g.allevi13','david_moore'),('g.allevi13','ella_miller'),('g.allevi13','emma_taylor'),('g.allevi13','fra'),('g.allevi13','g.allevi100'),('g.allevi13','grace_clark'),('g.allevi13','gsf'),('g.allevi13','jack_robinson'),('g.allevi13','mia_brown'),('g.allevi13','noah_carter'),('gsf','amy_carter'),('gsf','david_moore'),('gsf','emma_taylor'),('gsf','fra'),('gsf','grace_clark'),('gsf','zoe_jenkins'),('gsf2','fra'),('gsf2','g.allevi100'),('gsf2','g.allevi13'),('gsf2','gsf'),('jackson84','max_wilson'),('jackson84','sarah_johnson'),('max_wilson','emma_taylor'),('max_wilson','owen_hall'),('owen_hall','alex_cook'),('owen_hall','ella_miller'),('owen_hall','sarah_johnson'),('sarah_johnson','alex_cook'),('sarah_johnson','david_moore'),('sarah_johnson','jackson84'),('sophie_davis','david_moore'),('user123','jackson84'),('user456','lily22');
 /*!40000 ALTER TABLE `friendship` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -55,14 +47,14 @@ DROP TABLE IF EXISTS `match`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `match` (
-                         `idMatch` int auto_increment NOT NULL,
-                         `User1` varchar(45) NOT NULL,
-                         `User2` varchar(45) NOT NULL,
-                         `User3` varchar(45) NOT NULL,
-                         `Score` int NOT NULL,
-                         `Timestamp` datetime NOT NULL,
-                         PRIMARY KEY (`idMatch`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `idMatch` int NOT NULL AUTO_INCREMENT,
+  `Team1` varchar(200) DEFAULT NULL,
+  `Team2` varchar(200) DEFAULT NULL,
+  `ScoreTeam1` int DEFAULT NULL,
+  `ScoreTeam2` int DEFAULT NULL,
+  `Timestamp` datetime DEFAULT NULL,
+  PRIMARY KEY (`idMatch`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -71,7 +63,7 @@ CREATE TABLE `match` (
 
 LOCK TABLES `match` WRITE;
 /*!40000 ALTER TABLE `match` DISABLE KEYS */;
-INSERT INTO `match` VALUES (1,'user123','jackson84','lily22',1,'2023-01-01 12:30:45'),(2,'user456','lily22','mike_jones',7,'2023-01-02 14:20:30'),(3,'jackson84','max_wilson','sophie_davis',12,'2023-01-03 16:15:22'),(4,'sophie_davis','david_moore','emma_taylor',7,'2023-01-04 18:10:15'),(5,'max_wilson','emma_taylor','alex_cook',8,'2023-01-05 20:05:12'),(6,'sarah_johnson','jackson84','owen_hall',9,'2023-01-06 22:00:00'),(7,'emma_taylor','owen_hall','ella_miller',5,'2023-01-07 23:55:45'),(8,'jackson84','sarah_johnson','fra',4,'2023-01-08 01:50:30'),(9,'max_wilson','owen_hall','sarah_johnson',3,'2023-01-09 03:45:22'),(10,'owen_hall','ella_miller','luke_smith',5,'2023-01-10 05:40:15'),(11,'sarah_johnson','alex_cook','mia_brown',5,'2023-01-11 07:35:12'),(12,'fra','owen_hall','grace_clark',6,'2023-01-12 09:30:00'),(13,'owen_hall','sarah_johnson','zoe_jenkins',2,'2023-01-13 11:25:45'),(14,'alex_cook','mia_brown','noah_carter',8,'2023-01-14 13:20:30'),(15,'ella_miller','luke_smith','olivia_white',9,'2023-01-15 15:15:22'),(16,'fra','noah_carter','david_moore',7,'2023-01-16 17:10:15'),(17,'owen_hall','alex_cook','emma_taylor',9,'2023-01-17 19:05:12'),(18,'sarah_johnson','david_moore','max_wilson',4,'2023-01-18 21:00:00'),(19,'alex_cook','ella_miller','owen_hall',8,'2023-01-19 22:55:45'),(20,'fra','zoe_jenkins','mia_brown',9,'2023-01-20 00:50:30');
+INSERT INTO `match` VALUES (1,'[fra, fra2]','[gsf, cia]',0,0,'2024-04-05 12:02:24'),(2,'[fra, fra2]','[gsf, cia]',0,0,'2024-04-05 12:02:24'),(3,'[fra, fra2]','[gsf, cia]',0,0,'2024-04-05 12:12:00'),(4,'[fra, fra2]','[gsf, cia]',0,0,'2024-04-05 12:12:00'),(5,'[fra, fra2]','[gsf, cia]',0,0,'2024-04-05 12:12:00');
 /*!40000 ALTER TABLE `match` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -83,11 +75,11 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-                        `Username` varchar(45) NOT NULL,
-                        `Name` varchar(45) NOT NULL,
-                        `Surname` varchar(45) NOT NULL,
-                        `Password` varchar(100) NOT NULL,
-                        PRIMARY KEY (`Username`)
+  `Username` varchar(45) NOT NULL,
+  `Name` varchar(45) NOT NULL,
+  `Surname` varchar(45) NOT NULL,
+  `Password` varchar(100) NOT NULL,
+  PRIMARY KEY (`Username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -97,16 +89,25 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES ('gsf','Gaetano', 'Sferrazza', 'g'),('alex_cook','Alex','Cook','OceanWave456'),('amy_carter','Amy','Carter','Garden789'),('david_moore','David','Moore','Moonlight789'),('ella_miller','Ella','Miller','SweetDreams123'),('emma_taylor','Emma','Taylor','Butterfly123'),('grace_clark','Grace','Clark','PurpleRain123'),('jack_robinson','Jack','Robinson','CoffeeTime456'),('jackson84','Jackson','Smith','Secure123!'),('fra','Francesco','Bruno','f'),('lily22','Lily','Williams','Rainbow456'),('luke_smith','Luke','Smith','GoldenSun456'),('max_wilson','Max','Wilson','BlueSky123'),('mia_brown','Mia','Brown','HappyDay123'),('mike_jones','Mike','Jones','Summer2023!'),('noah_carter','Noah','Carter','SunnyDay789'),('olivia_white','Olivia','White','HappyPlace456'),('owen_hall','Owen','Hall','Starlight456'),('sophie_davis','Sophie','Davis','Sunflower456'),('user1','John','Doe','pass123'),('user10','Emma','Anderson','access'),('user123','Sarah','Johnson','P@ssw0rd123'),('user2','Alice','Smith','qwerty'),('user3','Bob','Johnson','secret'),('user4','Eva','Williams','mypassword'),('user5','Charlie','Brown','letmein'),('user6','Olivia','Davis','p@ssw0rd'),('user7','Michael','Miller','secure123'),('user8','Sophia','Moore','password123'),('user9','Liam','Taylor','123456'),('zoe_jenkins','Zoe','Jenkins','MountainView123');
+INSERT INTO `user` VALUES ('alex_cook','Alex','Cook','OceanWave456'),('amy_carter','Amy','Carter','Garden789'),('cia','ciaone','ciaonissimo','c'),('cia2','ciao','salve','c'),('david_moore','David','Moore','Moonlight789'),('ella_miller','Ella','Miller','SweetDreams123'),('emma_taylor','Emma','Taylor','Butterfly123'),('fra','Francesco','Bruno','f'),('fra2','francesco','brunos','f'),('g.allevi100','Giovanni','Allevi','g'),('g.allevi13','Giovanni','Allevi','g'),('grace_clark','Grace','Clark','PurpleRain123'),('gsf','Gaetano','Sferrazza','g'),('gsf2','gaetano','sferrazza','g'),('jack_robinson','Jack','Robinson','CoffeeTime456'),('jackson84','Jackson','Smith','Secure123!'),('lily22','Lily','Williams','Rainbow456'),('luke_smith','Luke','Smith','GoldenSun456'),('max_wilson','Max','Wilson','BlueSky123'),('mia_brown','Mia','Brown','HappyDay123'),('mike_jones','Mike','Jones','Summer2023!'),('noah_carter','Noah','Carter','SunnyDay789'),('olivia_white','Olivia','White','HappyPlace456'),('owen_hall','Owen','Hall','Starlight456'),('sophie_davis','Sophie','Davis','Sunflower456'),('user1','John','Doe','pass123'),('user10','Emma','Anderson','access'),('user123','Sarah','Johnson','P@ssw0rd123'),('user2','Alice','Smith','qwerty'),('user3','Bob','Johnson','secret'),('user4','Eva','Williams','mypassword'),('user5','Charlie','Brown','letmein'),('user6','Olivia','Davis','p@ssw0rd'),('user7','Michael','Miller','secure123'),('user8','Sophia','Moore','password123'),('user9','Liam','Taylor','123456'),('zoe_jenkins','Zoe','Jenkins','MountainView123');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
-Use taboo;
-
-DROP FUNCTION IF EXISTS userExists;
-DELIMITER //
-CREATE FUNCTION userExists(username VARCHAR(45))
-    RETURNS BOOLEAN DETERMINISTIC
+--
+-- Dumping routines for database 'taboo'
+--
+/*!50003 DROP FUNCTION IF EXISTS `userExists` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` FUNCTION `userExists`(username VARCHAR(45)) RETURNS tinyint(1)
+    DETERMINISTIC
 BEGIN
     DECLARE exists_ INT;
 
@@ -115,24 +116,20 @@ BEGIN
     WHERE U.username = username;
 
     RETURN exists_ > 0;
-END//
-
+END ;;
 DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-Use taboo;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-DROP FUNCTION IF EXISTS userExists;
-DELIMITER //
-CREATE FUNCTION userExists(username VARCHAR(45))
-    RETURNS BOOLEAN DETERMINISTIC
-BEGIN
-    DECLARE exists_ INT;
-
-    SELECT COUNT(*) INTO exists_
-    FROM user as U
-    WHERE U.username = username;
-
-    RETURN exists_ > 0;
-END//
-
-DELIMITER ;
+-- Dump completed on 2024-04-05 12:19:35

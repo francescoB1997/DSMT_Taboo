7ab9dec11e66fdcf7d379fc90bfabe1ae7d02dda
g.sferr <94866345+g-sferr@users.noreply.github.com>
2024-04-05 14:11:45 +0200

    addNewMatch function to execute query to insert a match into MySQL DB added

--- src/main/java/it/unipi/dsmt/dsmt_taboo/DAO/MatchDAO.java
+++ src/main/java/it/unipi/dsmt/dsmt_taboo/DAO/MatchDAO.java
@@ -2,14 +2,40 @@ package it.unipi.dsmt.dsmt_taboo.DAO;
 
 import it.unipi.dsmt.dsmt_taboo.model.DTO.MatchDTO;
 
+import java.sql.*;
+
 public class MatchDAO extends BaseDAO
 {
     public MatchDAO() { }
 
-    public void addNewMatch()
+    public boolean addNewMatch(MatchDTO match)
     // Insert in BD a new Match
     {
+        String insertQuery = "INSERT INTO " + DB_NAME + ".match " +
+                "(Team1, Team2, ScoreTeam1, ScoreTeam2, Timestamp) " + "VALUES (?, ?, ?, ?, ?)";
 
+        Timestamp timestamp = Timestamp.valueOf(match.getMatchId());
+
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.
+                     prepareStatement(insertQuery, PreparedStatement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.setString(1, match.getInviterTeam().toString());
+            preparedStatement.setString(2, match.getRivalTeam().toString());
+            preparedStatement.setInt(3, match.getScoreInviterTeam());
+            preparedStatement.setInt(4, match.getScoreRivalTeam());
+            preparedStatement.setTimestamp(5, timestamp);
+
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            if (rowsAffected > 0)
+                return true;
+            else
+                return false;
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return false;
+        }
     }
 
     public void getAllUserMatches(String username)

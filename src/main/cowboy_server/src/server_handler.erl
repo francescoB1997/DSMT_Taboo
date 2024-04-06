c2f5a100d35bd65205716e2efd1fdab91394ca25
francescoB1997 <francesco.0297@hotmail.it>
2024-04-06 10:52:08 +0200

    Modificata la view della pagina di gioco --> divisa in due l'area di gioco.
    Aggiunto la visualizzazione dinamica della TabooCard nella pagina tabooGamePage.html
    
    Aggiunta lato erlang la verifica della tabooWrod "una sola riga di codice"

--- src/main/cowboy_server/src/server_handler.erl
+++ src/main/cowboy_server/src/server_handler.erl
@@ -2,8 +2,7 @@
 -export([init/2, websocket_handle/2, websocket_info/2]).
 
     %% Formato dello Stato del Server:  {User, Role, Player1, WordList}
-init (Req, State) -> { cowboy_websocket, Req, { "", "", [], "" , [] } }.
-
+init (Req, State) -> { cowboy_websocket, Req, { "", "", [], [] , [] } }.
 
 websocket_handle(Frame = {text, JsonMsg}, State = {Username, Role, FriendList, GenericMessage, TabooCard}) ->
 %io:format("[Taboo WebSocket Handler]: => Frame: ~p, State: ~p~n", [JsonMsg, State]),
@@ -12,32 +11,34 @@ websocket_handle(Frame = {text, JsonMsg}, State = {Username, Role, FriendList, G
 	{Response, UpdatedState} =
 		if
 			UserAction == <<"login">> ->
-				S = player_handler:login(DecodedJson, State),
-				{Frame, S};
+				player_handler:login(DecodedJson, State);
 
-            UserAction == <<"start">> ->
-                player_handler:start(DecodedJson, State);
+            UserAction == <<"assignTabooCard">> ->
+                player_handler:assignTabooCard(State);
 
-            UserAction == <<"send_msg_to_friends">> ->
-                S = player_handler:send_msg_to_friends(DecodedJson, State),
+            UserAction == <<"startGame">> ->
+                S = player_handler:wakeUpAllGuessers(State),
                 {Frame, S};
 
+            UserAction == <<"send_msg_to_friends">> ->
+                player_handler:send_msg_to_friends(DecodedJson, State);
+
             UserAction == <<"attemptGuessWord">> ->
-                player_handler:attemptGuessWord(DecodedJson, State)
+                player_handler:attemptGuessWord(DecodedJson, State);
+
+            UserAction == <<"keepAlive">> ->
+                io:format("keepAlive di ~p~n", [Username]),
+                {Frame, State}
+                %player_handler:passTabooCard(State);
 
         end,
-            %true -> io:format("[Taboo WebSocket Handler]: UserAction non riconosciuta -> ~p~n", [UserAction])
-		%%	UserAction == <<"word">> ->
-		%%		S = player_handler:word(DecodedJson, State),
-		%%		{Frame, S};
-		%%	UserAction == <<"trigger_wait">> ->
-		%%		player_handler:wait_for_messages(State);
-		%%	UserAction == <<"guess_word">> ->
-		%%		player_handler:guess(DecodedJson, State)
-
 	%io:format("~p Response ~p~n", [Username,Response]),
 	{reply, [Response], UpdatedState}.
 
+    websocket_info( { start }, State = {Username, Role, FriendList, GenericMessage, TabooCard}) ->
+        io:format("Invocata websocket_info da parte di ~p~n", [Username]),
+        JsonMessage = jsx:encode([{<<"action">>, wakeUpGuesser}]),
+        {[{text, JsonMessage}], State};
 
 	websocket_info( {msgFromFriend, MsgFromFriend}, State) ->
     	JsonMessage = jsx:encode([{<<"action">>, msgFromFriend}, {<<"msg">>, MsgFromFriend}]),
@@ -47,8 +48,9 @@ websocket_handle(Frame = {text, JsonMsg}, State = {Username, Role, FriendList, G
                     State = {Username, Role, FriendList, GenericMessage, [AttemptedWord, TabooWord1, TabooWord2, TabooWord3, TabooWord4, TabooWord5]} ) ->
         Result = true,
         RequesterPID ! {resultAttemptGuessWord, Result},
-        JsonMessage = jsx:encode([{<<"action">>, attemptGuessWord}, {<<"msg">>, true}]),
-        {[{text, JsonMessage}], State};
+        NewTabooCard = player_handler:getRandomTabooCard(),
+        JsonMessage = jsx:encode([{<<"action">>, attemptGuessWord}, {<<"msg">>, true}, {<<"newTabooCard">>, NewTabooCard}]),
+        {[{text, JsonMessage}], {Username, Role, FriendList, GenericMessage, NewTabooCard}};
 
     websocket_info( {attemptGuessWord, RequesterPID, AttemptedWord},
                     State = {Username, Role, FriendList, GenericMessage, [Word, TabooWord1, TabooWord2, TabooWord3, TabooWord4, TabooWord5]} ) ->
@@ -58,5 +60,5 @@ websocket_handle(Frame = {text, JsonMsg}, State = {Username, Role, FriendList, G
         {[{text, JsonMessage}], State};
 
     websocket_info(Info, State) ->
-    	io:format("Taboo:websocket_info(Info, State) => Received info ~p~n", [Info]),
+    	io:format("Taboo:websocket_info(Info, State) => Received info ~p~n e STATE ~p~n", [Info, State]),
     	{ok, State}.

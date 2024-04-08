8c9948e7fa89449d59f64fc112b39f204c1faf73
g.sferr <94866345+g-sferr@users.noreply.github.com>
2024-04-08 21:35:41 +0200

    javascript file for Page welcome page for Admin User Created

---
+++ src/main/resources/static/javascript/adminHomePage.js
@@ -0,0 +1,71 @@
+const username = sessionStorage.getItem("userLog");
+
+$(document).ready(function ()
+{
+    if(!checkLogin())
+    {
+        location.href = "../";
+        return;
+    }
+    setWelcomeText();
+    document.getElementById('logoutBtn').onclick = function (e) { onClickListenerBtnLogout(); };
+    document.getElementById('browesGamesBtn').onclick = function (e) { onClickListenerBtnBrowesGames(); };
+    document.getElementById('viewUsersBtn').onclick = function (e) { onClickListenerBtnViewUsers(); };
+});
+
+function checkLogin()
+{
+    if(!username)
+    {
+        alert("You're not logged as ADMIN");
+        return false;
+    }
+    return true;
+}
+
+function setWelcomeText()
+{
+    let divWelcome = document.getElementById("h1WelcomeDiv");
+    divWelcome.innerHTML = "Welcome to Administrator Page";
+}
+
+function onClickListenerBtnLogout()
+{
+    let username = sessionStorage.getItem("userLog");
+    $.ajax({
+        url: "http://localhost:8080/logout",
+        type: "POST",
+        data: username,
+        dataType: "text",
+        contentType: 'application/text',
+        success: function ()
+        {
+            sessionStorage.removeItem("userLog");
+            location.href = "../";
+        },
+        error: function (xhr)
+        {
+            let serverResponse = JSON.parse(xhr.responseText);
+            alert(serverResponse.responseMessage);
+            location.href = "../";
+        }
+    });
+}
+
+function onClickListenerBtnBrowesGames()
+{
+    //Contattare servlet per la creazione del team, oppure reindirizzare verso una createTeams.html
+    //in cui selezionare da una lista di AMICI ONLINE quelli da inserire nella squadra (se è semplice, sarebbbe carino il drag and drop)
+    location.href = "../matchPage.html";
+}
+
+function onClickListenerBtnViewUsers()
+{
+    //Fare una chiamata asincrona AJAX per ottenere la lista degli amici, ossia un JSON con una lista di nomi
+    // e per ognuno, ci vede essere l'info se è Online o meno.
+    // Per ogni amico, va creato a runTime un elemento HTML per mostrarlo.
+
+    location.href = "../uersListPage.html";
+    return;
+}
+

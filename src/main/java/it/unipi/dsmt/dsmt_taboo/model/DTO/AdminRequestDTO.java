4b3308e9bcf01dc3cbfb06ce04d1876d531f7676
g.sferr <94866345+g-sferr@users.noreply.github.com>
2024-04-08 21:36:13 +0200

    username attribute + constructors added

--- src/main/java/it/unipi/dsmt/dsmt_taboo/model/DTO/AdminRequestDTO.java
+++ src/main/java/it/unipi/dsmt/dsmt_taboo/model/DTO/AdminRequestDTO.java
@@ -2,8 +2,17 @@ package it.unipi.dsmt.dsmt_taboo.model.DTO;
 
 public class AdminRequestDTO
 {
+
+    private String username;
     private String password;
     private String parameter;
+
+    public AdminRequestDTO() {}
+    public AdminRequestDTO(String username, String password, String parameter) {
+        this.username = username;
+        this.password = password;
+        this.parameter = parameter;
+    }
 
     public String getPassword() { return this.password; }
 
@@ -12,4 +21,11 @@ public class AdminRequestDTO
     public String getParameter() { return this.parameter; }
 
     public void setParameter(String parameter) { this.parameter = parameter; }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }

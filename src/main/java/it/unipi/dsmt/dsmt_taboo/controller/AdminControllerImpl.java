eba713c72b04c444d17e6b185b4631cdc5f854a7
g.sferr <94866345+g-sferr@users.noreply.github.com>
2024-04-08 21:40:06 +0200

    Added @RestController annotation to solve requests issues + checklogin added for methods deleteUser() and getAllSignedUsers()

--- src/main/java/it/unipi/dsmt/dsmt_taboo/controller/AdminControllerImpl.java
+++ src/main/java/it/unipi/dsmt/dsmt_taboo/controller/AdminControllerImpl.java
@@ -1,70 +1,129 @@
 package it.unipi.dsmt.dsmt_taboo.controller;
 
+import it.unipi.dsmt.dsmt_taboo.DAO.FriendDAO;
 import it.unipi.dsmt.dsmt_taboo.DAO.MatchDAO;
 import it.unipi.dsmt.dsmt_taboo.DAO.UserDAO;
-import it.unipi.dsmt.dsmt_taboo.model.DTO.AdminRequestDTO;
-import it.unipi.dsmt.dsmt_taboo.model.DTO.MatchDTO;
-import it.unipi.dsmt.dsmt_taboo.model.DTO.ServerResponseDTO;
-import it.unipi.dsmt.dsmt_taboo.model.DTO.UserDTO;
+import it.unipi.dsmt.dsmt_taboo.model.DTO.*;
 import it.unipi.dsmt.dsmt_taboo.utility.Constant;
+import it.unipi.dsmt.dsmt_taboo.utility.SessionManagement;
 import org.apache.catalina.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
 public class AdminControllerImpl implements AdminControllerInterface
 {
     @PostMapping("/deleteUser")
     @Override
-    public ResponseEntity<ServerResponseDTO<Boolean>> deleteUser(AdminRequestDTO userToDeleteRequest)
+    public ResponseEntity<ServerResponseDTO<Integer>> deleteUser(@RequestBody AdminRequestDTO userToDeleteRequest)
     {
         HttpStatus responseHttp;
-        ServerResponseDTO<Boolean> userToDeleteResponse = null;
+        ServerResponseDTO<Integer> userToDeleteResponse;
+        boolean checkLogin = SessionManagement.getInstance().
+                isUserLogged(userToDeleteRequest.getUsername());
+        int requestStatus = 0;
 
-        if(!checkAdmin(userToDeleteRequest))
-            responseHttp = HttpStatus.UNAUTHORIZED;
-        else
+        if(checkLogin)
         {
-            UserDAO userDAO = new UserDAO();
-            Boolean removeOK = userDAO.removeUser(userToDeleteRequest.getParameter());
-            if(removeOK)
-            {
-                responseHttp = HttpStatus.OK;
-                userToDeleteResponse = new ServerResponseDTO<>(true);
+            if(!checkAdmin(userToDeleteRequest)) {
+                responseHttp = HttpStatus.UNAUTHORIZED;
+            } else {
+
+                UserDAO userDAO = new UserDAO();
+                boolean removeOK = userDAO.removeUser(userToDeleteRequest.getParameter());
+                if(removeOK)
+                {
+                    System.out.println("\nThe user "
+                            + userToDeleteRequest.getParameter() +
+                            " has been successfully removed\n");
+                    responseHttp = HttpStatus.OK;
+                    userToDeleteResponse = new ServerResponseDTO<>(requestStatus);
+                }
+                else
+                {
+                    System.out.println("\nError occurred during remove operation." +
+                            userToDeleteRequest.getParameter() +
+                            " has NOT been removed from your friend list\n");
+                    requestStatus++;
+                    responseHttp = HttpStatus.BAD_REQUEST;
+                    userToDeleteResponse = new ServerResponseDTO<>(requestStatus);
+                }
+                return new ResponseEntity<>(userToDeleteResponse, responseHttp);
             }
-            else
-            {
-                responseHttp = HttpStatus.BAD_REQUEST;
-                userToDeleteResponse = new ServerResponseDTO<>(false);
-            }
+
+            userToDeleteResponse = new ServerResponseDTO<>(null);
+            return new ResponseEntity<>(userToDeleteResponse, responseHttp);
+
+        } else {
+
+            System.out.println("\nAdminController: getUsers request from a NonLogged user\n");
+            userToDeleteResponse = new ServerResponseDTO<>(null);
+            responseHttp = HttpStatus.UNAUTHORIZED;
         }
+
         return new ResponseEntity<>(userToDeleteResponse, responseHttp);
     }
 
-    @PostMapping("/getAllUsers")
+    @PostMapping("/getUsers")
     @Override
-    public ResponseEntity<ServerResponseDTO<List<UserDTO>>> getAllSignedUsers(AdminRequestDTO getAllUserRequest)
+    public ResponseEntity<ServerResponseDTO<List<UserDTO>>>
+    getAllSignedUsers(@RequestBody AdminRequestDTO getUserRequest)
     {
         HttpStatus responseHttp;
-        ServerResponseDTO<List<UserDTO>> getAllUserResponse = null;
+        ServerResponseDTO<List<UserDTO>> getUserResponse = null;
+        boolean checkLogin = SessionManagement.getInstance().
+                isUserLogged(getUserRequest.getUsername());
 
-        if(!checkAdmin(getAllUserRequest))
-            responseHttp = HttpStatus.UNAUTHORIZED;
-        else
+        if(checkLogin)
         {
-            UserDAO userDAO = new UserDAO();
-            getAllUserResponse = new ServerResponseDTO<>( userDAO.globalSearchUser("")); // if you pass the EmptyString, the globalUserSearch returns all users
-            responseHttp = HttpStatus.OK;
+            if(!checkAdmin(getUserRequest)) {
+                responseHttp = HttpStatus.UNAUTHORIZED;
+            } else {
+
+                UserDAO userDAO = new UserDAO();
+                List<UserDTO> userList = userDAO.globalSearchUser(getUserRequest.getParameter());
+
+                if(userList.isEmpty())
+                {
+                    if(!getUserRequest.getParameter().equals("")) {
+                        System.out.println("\nAdminController: - NOT FOUND - Database NOT contains the user: "
+                                           + getUserRequest.getParameter() + "\n");
+                    }
+                    getUserResponse = new ServerResponseDTO<>(null);
+
+                } else {
+
+                    if(!getUserRequest.getParameter().equals("")) {
+                        System.out.println("\nAdminController: - OK - Database contains the user: "
+                                           + getUserRequest.getParameter() + "\n");
+                    }
+                    getUserResponse = new ServerResponseDTO<>(userList);
+                }
+
+                responseHttp = HttpStatus.OK;
+                return new ResponseEntity<>(getUserResponse, responseHttp);
+            }
+
+            return new ResponseEntity<>(getUserResponse, responseHttp);
+
+        } else {
+            System.out.println("\nAdminController: getUsers request from a NonLogged user\n");
+            getUserResponse = new ServerResponseDTO<>(null);
+            responseHttp = HttpStatus.UNAUTHORIZED;
         }
-        return new ResponseEntity<>(getAllUserResponse, responseHttp);
+
+        return new ResponseEntity<>(getUserResponse, responseHttp);
     }
 
     @PostMapping("/getAllMatches")
     @Override
-    public ResponseEntity<ServerResponseDTO<List<MatchDTO>>> getAllMatches(AdminRequestDTO getAllMatchesRequest) {
+    public ResponseEntity<ServerResponseDTO<List<MatchDTO>>> getAllMatches(@RequestBody AdminRequestDTO getAllMatchesRequest) {
 
         HttpStatus responseHttp;
         ServerResponseDTO<List<MatchDTO>> getAllMatchesResponse = null;

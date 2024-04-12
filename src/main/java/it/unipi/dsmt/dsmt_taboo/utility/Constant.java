3ad8eceebb8132fe24864f6ece6e3bc6493d5e4e
g.sferr <94866345+g-sferr@users.noreply.github.com>
2024-04-12 14:38:32 +0200

    latch settato a 4

--- src/main/java/it/unipi/dsmt/dsmt_taboo/utility/Constant.java
+++ src/main/java/it/unipi/dsmt/dsmt_taboo/utility/Constant.java
@@ -4,7 +4,7 @@ public final class Constant
 {
     public enum PlayerRole{Prompter, Guesser, Controller};
 
-    public static final Integer NUMBER_OF_PLAYERS_FOR_MATCH = 6; // Number of all Player (both teams)
+    public static final Integer NUMBER_OF_PLAYERS_FOR_MATCH = 4; // Number of all Player (both teams)
     public static final String usernameAdmin = "admin";
     public static final String passwordAdmin = "admin";
 }

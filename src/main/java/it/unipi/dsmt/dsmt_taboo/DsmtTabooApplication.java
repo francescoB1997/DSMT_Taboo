7659d3c881077a398f78bc417b33ebc5626a5955
g.sferr <94866345+g-sferr@users.noreply.github.com>
2024-04-17 10:31:22 +0200

    aggiunto cors filter

--- src/main/java/it/unipi/dsmt/dsmt_taboo/DsmtTabooApplication.java
+++ src/main/java/it/unipi/dsmt/dsmt_taboo/DsmtTabooApplication.java
@@ -1,13 +1,26 @@
 package it.unipi.dsmt.dsmt_taboo;
 
+import it.unipi.dsmt.dsmt_taboo.utility.FilterManagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DsmtTabooApplication {
     public static void main(String[] args)
     {
         SpringApplication.run(DsmtTabooApplication.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean corsFilterRegistration() {
+        FilterRegistrationBean registrationBean =
+                new FilterRegistrationBean(new FilterManagement());
+        registrationBean.setName("CORS Filter");
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(1);
+        return registrationBean;
     }
 }
 

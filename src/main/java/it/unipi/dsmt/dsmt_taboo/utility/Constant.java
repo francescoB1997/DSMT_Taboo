2583b00a4835a384cd2d4ee8d5e75395359837cf
g.sferr <94866345+g-sferr@users.noreply.github.com>
2024-04-20 19:33:08 +0200

    Enum for Roles Removed

--- src/main/java/it/unipi/dsmt/dsmt_taboo/utility/Constant.java
+++ src/main/java/it/unipi/dsmt/dsmt_taboo/utility/Constant.java
@@ -2,8 +2,6 @@ package it.unipi.dsmt.dsmt_taboo.utility;
 
 public final class Constant
 {
-    public enum PlayerRole{Prompter, Guesser, Controller};
-
     public static final Integer NUMBER_OF_PLAYERS_FOR_MATCH = 4; // Number of all Player (both teams)
     public static final String usernameAdmin = "admin";
     public static final String passwordAdmin = "admin";

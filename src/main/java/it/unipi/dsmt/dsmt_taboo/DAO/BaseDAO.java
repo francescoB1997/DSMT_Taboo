2020bd46a1174d2578c611b79e04773403fd84b2
g.sferr <94866345+g-sferr@users.noreply.github.com>
2024-04-21 11:24:54 +0200

    Commented on and cleaned up the DAO class files

--- src/main/java/it/unipi/dsmt/dsmt_taboo/DAO/BaseDAO.java
+++ src/main/java/it/unipi/dsmt/dsmt_taboo/DAO/BaseDAO.java
@@ -3,15 +3,24 @@ package it.unipi.dsmt.dsmt_taboo.DAO;
 import java.sql.Connection;
 import java.sql.SQLException;
 import javax.sql.DataSource;
-import javax.xml.crypto.Data;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
 import it.unipi.dsmt.dsmt_taboo.exceptions.DatabaseNotReachableException;
-import org.springframework.core.io.buffer.DataBufferLimitException;
 
 
 public class BaseDAO
 {
+    /**
+     * This class provides basic Data Access Object (DAO) functionality for connecting to
+     * and communicating with the MySQL database. It includes methods to set up a data source
+     * and retrieve a connection to the database. The class initializes the data source upon
+     * instantiation and provides a getConnection() method to obtain a database connection.
+     * It handles exceptions related to database connectivity, such as DatabaseNotReachableException,
+     * by throwing an appropriate exception when the connection cannot be established.
+     * The class also defines constants for database configuration parameters such as database name,
+     * IP address, URL, username, and password.
+     */
+
     public static final String DB_NAME = "taboo";
     private static final String DB_IP = "10.2.1.131";
     private static final String DB_URL = "jdbc:mysql://" + DB_IP + ":3306/" + DB_NAME;
@@ -23,10 +32,6 @@ public class BaseDAO
         dataSource = this.setupDataSource();
         if (dataSource == null)
             System.out.println("BaseFunctionalitiesDB: Connection to DB FAILED!");
-    }
-
-    public void closeConnection() {
-        // Non è necessario chiudere la connessione poiché il DataSource gestirà le connessioni
     }
 
     private DataSource setupDataSource()
@@ -48,59 +53,7 @@ public class BaseDAO
             System.out.println("BaseFunctionalitiesDB: Can't get connection");
             if(e.getSQLState().equals("08S01"))
                 throw new DatabaseNotReachableException();
-            //e.printStackTrace();
             return null;
         }
     }
 }
-
-/*
-Precendete versione della BaseDAO. Si utilizzava la classe DriverManager per ottenre la connessione verso
-il DB ma non è ThreadSafe, a differenza della DataScource.
-
-public class BaseDAO
-    // Base functionalities to interact with DB
-{
-    public static final String DB_NAME = "taboo";
-    private static final String DB_URL = "jdbc:mysql://10.2.1.111:3306/" + DB_NAME;
-    private static final String DB_USERNAME = "root";
-    private static final String DB_PASSWORD = "root";
-    private Connection connection = null;
-
-    public BaseDAO()
-    {
-        connection = this.getConnection();
-        if(connection == null)
-            System.out.println("BaseFunctionalitiesDB: Connection DB FAILED!");
-    }
-
-    public void closeConnection() {
-        try
-        {
-            if (connection != null && !connection.isClosed())
-                connection.close();
-        } catch (SQLException e) {
-            System.out.println("BaseFunctionalitiesDB: Can't close connection");
-            //e.printStackTrace();
-        }
-    }
-
-    public Connection getConnection()
-    {
-        try
-        {
-            if (connection == null || connection.isClosed())
-                connection = DriverManager.getConnection(this.DB_URL, this.DB_USERNAME, this.DB_PASSWORD);
-        }
-        catch (SQLException e)
-        {
-            //e.printStackTrace();
-            connection = null;
-        }
-        return connection;
-    }
-
-}
-
-
-*/

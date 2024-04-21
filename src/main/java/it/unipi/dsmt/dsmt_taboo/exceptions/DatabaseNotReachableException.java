423d62131066ef080300f04422b4aa26dc470d19
g.sferr <94866345+g-sferr@users.noreply.github.com>
2024-04-21 11:27:26 +0200

    Commented on and cleaned up the Exceptions class files.

--- src/main/java/it/unipi/dsmt/dsmt_taboo/exceptions/DatabaseNotReachableException.java
+++ src/main/java/it/unipi/dsmt/dsmt_taboo/exceptions/DatabaseNotReachableException.java
@@ -2,6 +2,13 @@ package it.unipi.dsmt.dsmt_taboo.exceptions;
 
 public class DatabaseNotReachableException extends Exception
 {
+    /**
+     * This class represents a checked exception that is thrown when the program is unable to reach the database.
+     * It is used to handle situations where communication with the database is not possible due to connection issues
+     * or misconfiguration. It extends the Exception class and provides a default message indicating that the database
+     * is unreachable.
+     */
+
     public DatabaseNotReachableException()
     {
         super("The DB is not reachable");
--- src/main/java/it/unipi/dsmt/dsmt_taboo/exceptions/UserNotExistsException.java
+++ src/main/java/it/unipi/dsmt/dsmt_taboo/exceptions/UserNotExistsException.java
@@ -2,6 +2,13 @@ package it.unipi.dsmt.dsmt_taboo.exceptions;
 
 public class UserNotExistsException extends Exception
 {
+    /**
+     * This class represents a checked exception that is thrown when a user does not exist in the system
+     * during a login attempt. It extends the Exception class and provides a default message indicating
+     * a login error due to incorrect username or password. This exception is used to handle situations
+     * where the user credentials provided during login do not match any existing user records in the system.
+     */
+
     public UserNotExistsException ()
     {
         super("Login Error. Please, check the Username or Password");

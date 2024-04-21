5eee729b830326b279a91a2598933fd509629799
g.sferr <94866345+g-sferr@users.noreply.github.com>
2024-04-21 11:27:01 +0200

    Commented on and cleaned up the Entity class files

--- src/main/java/it/unipi/dsmt/dsmt_taboo/model/entity/PendingMatch.java
+++ src/main/java/it/unipi/dsmt/dsmt_taboo/model/entity/PendingMatch.java
@@ -11,45 +11,75 @@ import java.util.stream.Collectors;
     In questo modo verrà creato un pendingMatch consistente con l'invito (se tutti accettano).
  */
 public class PendingMatch
-    //Classe che rappresenta un match pendente, ossia non ancora in attesa di risposta di accettazione/rifiuto di invito
 {
+
+    /**
+     * This class represents a pending match, which is a match that is not yet awaiting responses
+     * for invitation acceptance or rejection. It includes fields for a CountDownLatch object,
+     * which is used to synchronize the invitation process, and lists of usernames representing members
+     * of the inviting team and rival team waiting for responses. The class provides methods to add users
+     * to the waiting lists for both teams, ensuring that each user is added only once and that
+     * the CountDownLatch is appropriately decremented. Additionally, it includes a method to wake up all threads
+     * awaiting the completion of the invitation process. The class facilitates the management of
+     * pending match invitations within the application.
+     */
+
     private CountDownLatch latch;
     private ArrayList<String> inviterTeamWaiting;
     private ArrayList<String> rivalsTeamWaiting;
 
+    /**
+     * Constructs a new PendingMatch object with an initialized CountDownLatch
+     * and empty waiting lists for both teams.
+     */
     public PendingMatch()
     {
         this.rivalsTeamWaiting = new ArrayList<>();
         this.inviterTeamWaiting = new ArrayList<>();
         latch = new CountDownLatch( Constant.NUMBER_OF_PLAYERS_FOR_MATCH );
     }
+
+    /**
+     * Adds a new rival waiting for the match invitation, ensuring it is added only once.
+     * Decrements the CountDownLatch and awaits completion of the invitation process.
+     * @param usernameRival The username of the rival waiting for the invitation.
+     */
     public void addWaitingRival(String usernameRival)
-    // This function add a new RivalWaiting only if it does not waiting yet
     {
-        if (this.rivalsTeamWaiting.stream().filter(rivalWaiting -> rivalWaiting.equals(usernameRival)).collect(Collectors.toList()).isEmpty())
+        if (this.rivalsTeamWaiting.stream().filter(rivalWaiting ->
+                                            rivalWaiting.equals(usernameRival)).
+                                            collect(Collectors.toList()).isEmpty())
         {
             this.rivalsTeamWaiting.add(usernameRival);
             this.latch.countDown();
             System.out.println("[R] CountDown: " + this.latch.getCount());
         }
         try { this.latch.await(); }
-        catch (Exception e) { System.out.println("Errore eccezione await() [R] -> " + e.getMessage()); }
-        //System.out.println("ThreadR di [ " + usernameRival + " ] Sveglio");
+        catch (Exception e) { System.out.println("Error: await() exception [R] -> " + e.getMessage()); }
     }
 
+    /**
+     * Adds a new friend waiting for the match invitation, ensuring it is added only once.
+     * Decrements the CountDownLatch and awaits completion of the invitation process.
+     * @param usernameFriend The username of the friend waiting for the invitation.
+     */
     public void addWaitingFriend(String usernameFriend)
-    // This function add a new FriendWaiting only if it does not waiting yet
     {
-        if (this.inviterTeamWaiting.stream().filter(friendWaiting -> friendWaiting.equals(usernameFriend)).collect(Collectors.toList()).isEmpty())
+        if (this.inviterTeamWaiting.stream().filter(friendWaiting ->
+                                            friendWaiting.equals(usernameFriend)).
+                                            collect(Collectors.toList()).isEmpty())
         {
             this.inviterTeamWaiting.add(usernameFriend);
             this.latch.countDown();
             System.out.println("[F] CountDown: " + this.latch.getCount());
         }
         try { this.latch.await(); }
-        catch (Exception e) { System.out.println("Errore eccezione await() [F] -> " + e.getMessage()); }
+        catch (Exception e) { System.out.println("Error: await() exception [F] -> " + e.getMessage()); }
     }
 
+    /**
+     * Wakes up all threads awaiting the completion of the invitation process by decrementing the CountDownLatch.
+     */
     public void wakeUpAllThreads()
     {
         long round = this.latch.getCount();
@@ -57,7 +87,15 @@ public class PendingMatch
             this.latch.countDown();
     }
 
+    /**
+     * Retrieves the list of usernames representing members of the inviting team waiting for responses.
+     * @return The list of usernames of members of the inviting team waiting for responses.
+     */
     public ArrayList<String> getInviterTeamMember() { return this.inviterTeamWaiting; }
 
+    /**
+     * Retrieves the list of usernames representing members of the rival team waiting for responses.
+     * @return The list of usernames of members of the rival team waiting for responses.
+     */
     public ArrayList<String> getRivalsTeamMember() { return this.rivalsTeamWaiting; }
 }

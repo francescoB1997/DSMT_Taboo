093fda984740dc2027dc46c25a1f1215fc082762
g.sferr <94866345+g-sferr@users.noreply.github.com>
2024-04-21 11:23:37 +0200

    Commented and cleaned up Main Class

--- src/main/java/it/unipi/dsmt/dsmt_taboo/DsmtTabooApplication.java
+++ src/main/java/it/unipi/dsmt/dsmt_taboo/DsmtTabooApplication.java
@@ -7,12 +7,11 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class DsmtTabooApplication {
-    public static void main(String[] args)
-    {
+public class DsmtTabooApplication
+{
+    public static void main(String[] args) {
         SpringApplication.run(DsmtTabooApplication.class, args);
     }
-
     @Bean
     public FilterRegistrationBean corsFilterRegistration() {
         FilterRegistrationBean registrationBean =
@@ -22,5 +21,14 @@ public class DsmtTabooApplication {
         registrationBean.setOrder(1);
         return registrationBean;
     }
+
+    /**
+     * This class represents the main application class for the DSMT Taboo-Game Web Application.
+     * It is annotated with @SpringBootApplication to enable Spring Boot autoconfiguration.
+     * The main method initializes and runs the Spring application.
+     * Additionally, it includes a method corsFilterRegistration() annotated with @Bean,
+     * which registers the FilterManagement class as a CORS filter for handling cross-origin requests.
+     * The filter registration is configured to apply to all URL patterns and has an order of 1.
+     */
 }
 
